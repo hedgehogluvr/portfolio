15ce376a4740f4ac7e928ac176278490da91de80
hedgehogluvr <miafrancica@gmail.com>
2026-04-06 23:16:07 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function switchContent() {
+  const text = document.getElementById("text");
+  const images = document.getElementById("images");
+  const body = document.body;
+
+  text.classList.toggle("hidden");
+  images.classList.toggle("show");
+  body.classList.toggle("dark-mode");
+}
+const square = document.querySelector(".square");
+square.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
