46b9e687ee31a60bf5b8484feec804593383d1d9
Mia Francica <miafrancica@gmail.com>
2026-04-08 16:16:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ const square = document.querySelector(".square");
 square.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
+function switchContent() {
+  const text = document.getElementById("text");
+  const images = document.getElementById("images");
+
+  text.classList.toggle("hidden");
+  images.classList.toggle("show");
+
+  // THIS triggers background + hedgehog swap
+  document.body.classList.toggle("dark-mode");
+}
